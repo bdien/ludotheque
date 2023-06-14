0d90506da1d7f958a501c1760335589ff945e278
Benoît <benoit.dien@gmail.com>
2023-06-14 21:54:25 +0200

    PWA

--- src/webui/vite.config.ts
+++ src/webui/vite.config.ts
@@ -1,9 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "autoUpdate",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Ludo du Poisson-Lune",
+    short_name: "Ludothèque",
+    description: "Accèder à la ludothèque du Poisson-Lune d'Acigné",
+    icons: [
+      {
+        src: "/pwaicon-96-96.png",
+        sizes: "96x96",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+      {
+        src: "/pwaicon-192-192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+      {
+        src: "/pwaicon-512-512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#FFFFFF",
+    display: "fullscreen",
+    orientation: "portrait",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
   server: {
     proxy: {
       "/api": {
