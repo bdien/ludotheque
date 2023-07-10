bf3ac3e864f2a8c9e06db6e2c2055877d77a7c59
Benoît <benoit.dien@gmail.com>
2023-07-10 12:23:50 +0200

    Remember item list position

--- src/webui/src/theme.tsx
+++ src/webui/src/theme.tsx
@@ -1,25 +1,21 @@
 import { createTheme } from "@mui/material/styles";
-import { frFR } from "@mui/x-data-grid";
 
 // A custom theme for this app
-const theme = createTheme(
-  {
-    typography: {
-      fontSize: 13,
+const theme = createTheme({
+  typography: {
+    fontSize: 13,
+  },
+  palette: {
+    primary: {
+      main: "#556cd6",
     },
-    palette: {
-      primary: {
-        main: "#556cd6",
-      },
-      secondary: {
-        main: "#19857b",
-      },
-      background: {
-        default: "#F1F3F5",
-      },
+    secondary: {
+      main: "#19857b",
+    },
+    background: {
+      default: "#F1F3F5",
     },
   },
-  frFR,
-);
+});
 
 export default theme;
