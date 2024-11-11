687e623091d3788fce09b5f48e170de471eb59a0
Benoît <benoit.dien@gmail.com>
2024-11-11 17:15:44 +0100

    Fix pour la creation d'objet avec images

--- src/tests_api/test_pictures.py
+++ src/tests_api/test_pictures.py
@@ -12,6 +12,36 @@ app.dependency_overrides[auth_user] = fake_auth_user
 IMG_FILE = b"\xff\xd8\xff\xe0\x00\x10JFIF\x00\x01\x01\x01\x00H\x00H\x00\x00\xff\xdb\x00C\x00\x03\x02\x02\x02\x02\x02\x03\x02\x02\x02\x03\x03\x03\x03\x04\x06\x04\x04\x04\x04\x04\x08\x06\x06\x05\x06\t\x08\n\n\t\x08\t\t\n\x0c\x0f\x0c\n\x0b\x0e\x0b\t\t\r\x11\r\x0e\x0f\x10\x10\x11\x10\n\x0c\x12\x13\x12\x10\x13\x0f\x10\x10\x10\xff\xc9\x00\x0b\x08\x00\x01\x00\x01\x01\x01\x11\x00\xff\xcc\x00\x06\x00\x10\x10\x05\xff\xda\x00\x08\x01\x01\x00\x00?\x00\xd2\xcf \xff\xd9"
 
 
+def test_create_item_with_pictures(fakestorage):
+    img64 = "data:image/jpeg;base64," + base64.b64encode(IMG_FILE).decode()
+    response = client.post(
+        "/items", json={"name": "objet", "pictures": [img64]}, headers=AUTH_ADMIN
+    )
+    assert response.status_code == 200
+    newitem = response.json()
+    assert "id" in newitem
+
+    assert os.path.isfile(
+        f"{fakestorage}/img/jeu_00001_b2bb8775b7d5bf59c36c8637293a4602.webp"
+    )
+    assert os.path.isfile(
+        f"{fakestorage}/thumb/jeu_00001_b2bb8775b7d5bf59c36c8637293a4602.webp"
+    )
+
+    # Check in DB
+    with db:
+        item_db = Item.get_by_id(newitem["id"])
+        assert item_db.name == "objet"
+        assert item_db.pictures == ["jeu_00001_b2bb8775b7d5bf59c36c8637293a4602.webp"]
+
+    # Check in API
+    response = client.get(f"/items/{newitem['id']}")
+    assert response.status_code == 200
+    item = response.json()
+    assert item["name"] == "objet"
+    assert item["pictures"] == ["jeu_00001_b2bb8775b7d5bf59c36c8637293a4602.webp"]
+
+
 def test_item_add_picture(fakestorage):
     "Add two pictures"
     with db:
